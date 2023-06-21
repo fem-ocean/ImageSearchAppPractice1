aa79f929c938a4e94e997de3f4e8976d077d88f8
Femi <abiolaoshin@gmail.com>
2023-06-21 15:43:39 +0100

    latest changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //1. User types in a value in the input bar and we get the value
-// 2. When user click on the search button we use the input value to make the search and it gets the result.
-//It displays the result in the .search-results class div.
-//if the result page is more than 1, the show more button displays. If it is less, it doesnt.
+// 2. When user click on the search button, we would use the input value as a query to make the search to an API and get a result.
+//3. the result is displayed in the .search-results class div.
+//4.  if the result page is more than 1, the show more button displays. If it is not more than 1, it doesnt show.
 
 // IMPORTANT VARIABLES
 
@@ -18,48 +18,52 @@ let searchInputValue = "";
 function getImages() {
   searchInputValue = searchInput.value;
 
-  const API = `https://api.unsplash.com/search/photos/?query=${searchInputValue}&client_id=QGtQu40-81LdBQtPOjxktYTtKy3b1OIAfw-tsB7XELU&page=${page}&orientation=landscape`;
+  //form validation for empty input
+  if (searchInputValue === "") {
+    window.alert("Oops!....Empty query. You need to input a value");
+  } else {
+    const API = `https://api.unsplash.com/search/photos/?query=${searchInputValue}&client_id=QGtQu40-81LdBQtPOjxktYTtKy3b1OIAfw-tsB7XELU&page=${page}&orientation=landscape`;
 
-  //This means when this function is called and the page equals 1, remove whatever content is in the div. i.e make the div empty.
-  if (page === 1) {
-    searchResults.innerHTML = "";
-  }
+    //This means when this function is called and the page equals 1, remove whatever content is in the div. i.e make the div empty.
+    if (page === 1) {
+      searchResults.innerHTML = "";
+    }
 
-  fetch(API)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not okay");
-      }
-      return response.json();
-    })
-    .then((response) => {
-      console.log(response);
-      var imageData = response.results;
-      imageData.map((result) => {
-        console.log("Data mapping");
-        const searchResult = document.createElement("div");
-        searchResult.classList.add("search-result");
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-        image.alt = result.alt_description;
-        const anchor = document.createElement("a");
-        anchor.href = result.links.html;
-        anchor.target = "_blank";
-        anchor.textContent = result.alt_description;
+    // consume the API
+    fetch(API)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not okay");
+        }
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        var imageData = response.results;
+        imageData.map((result) => {
+          const searchResult = document.createElement("div");
+          searchResult.classList.add("search-result");
+          const image = document.createElement("img");
+          image.src = result.urls.small;
+          image.alt = result.alt_description;
+          const anchor = document.createElement("a");
+          anchor.href = result.links.html;
+          anchor.target = "_blank";
+          anchor.textContent = result.alt_description;
 
-        searchResult.append(image);
-        searchResult.append(anchor);
-        document.querySelector(".search-results").append(searchResult);
-      });
-    })
-    .catch((err) => console.log(err));
+          searchResult.append(image);
+          searchResult.append(anchor);
+          document.querySelector(".search-results").append(searchResult);
+        });
+      })
+      .catch((err) => console.log(err));
 
+    //Increase the page count and let show-more-button know that page count has increased.
+    page++;
 
-  //Increase the page count and let show-more-button know that page count has increased.
-  page++;
-
-  if (page > 1) {
-    document.getElementById("show-more-button").style.display = "block";
+    if (page > 1) {
+      document.getElementById("show-more-button").style.display = "block";
+    }
   }
 }
 
@@ -67,7 +71,6 @@ function getImages() {
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("Search button clicked");
   page = 1;
   getImages();
 });
